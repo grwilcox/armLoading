95ce36e6f0b0a646d87134c387683e700c5b584a
Graham Wilcox <53061984+grwilcox@users.noreply.github.com>
2020-02-01 22:30:14 -0500

    Update linkBuilder.m
    added documentation

--- linkBuilder.m
+++ linkBuilder.m
@@ -1,10 +1,23 @@
-function link = linkBuilder(name, mass, length, angle, linkArray, ~, k)
+% Initializes all characteristics of a link independent of the parameters
+% of the rest of the system (i.e. anything that doesn't need transforms)
+% 
+% name - string, plain identifier for link
+% id - int, unique numerical identifier for link, the i of a_ij
+% mass - mass of component in kg
+% length - distance a_ij between joint vectors S_i and S_j
+% angle - skew angle alpha_ij between joint vectors S_i and S_j
+% k - struct of gravity parameters
+%
+% link - struct of characteristics of the single link at hand
+
+
+function link = linkBuilder(name, id, mass, length, angle, k)
 link.name = name;
-link.id = numel(linkArray)+1;
+link.id = id;
 link.mass = mass;
 link.a = length;
 link.localVec = [link.a; 0; 0];
 link.alpha = angle;
-link.cmVec = link.localVec/2;
+link.cmLocal = link.localVec/2; % TODO: add cmLocal input argument
 link.gravForce = link.mass * k.g * -k.kHat;
 end
