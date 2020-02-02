d9129f2e812d07f5bc7cdbcd09d6dc16dd473a66
Graham Wilcox <53061984+grwilcox@users.noreply.github.com>
2020-02-02 18:15:19 -0500

    added ifs to ignore zero mass parts

--- plotMassLoads.m
+++ plotMassLoads.m
@@ -1,4 +1,6 @@
 % Plots vectors representing weight of components over existing arm plot. 
+% Vectors originate from specified CoM of component. 
+% Only nonzero loads are plotted. 
 % >>> Must be called AFTER plotArm.
 % 
 % linkArray - structure array of link parameters
@@ -8,14 +10,18 @@
 function plotMassLoads(linkArray,jointArray,scale)
 hold on
 for n = 1:numel(jointArray)
-    cmJ = jointArray(n).cmGlobal;   % coordinates of joint CoM
+    if jointArray(n).mass~=0    % masslass components ignored
+        cmJ = jointArray(n).cmGlobal;   % coordinates of joint CoM
+        FgJ = cmJ + jointArray(n).gravForce*scale;  % translate load vector
+        plot3([cmJ(1) FgJ(1)],[cmJ(2) FgJ(2)],[cmJ(3) FgJ(3)],...
+                'd-.r','MarkerIndices',2,'LineWidth',.75,'MarkerSize',9)
+    end
+    if linkArray(n).mass~=0
     cmL = linkArray(n).cmGlobal;    % coordinates of link CoM
-    FgJ = cmJ + jointArray(n).gravForce*scale; % translate force vectors
     FgL = cmL + linkArray(n).gravForce*scale;
-    plot3([cmJ(1) FgJ(1)],[cmJ(2) FgJ(2)],[cmJ(3) FgJ(3)],'d-.r',...
-        'MarkerIndices',2,'LineWidth',.75,'MarkerSize',9)
-    plot3([cmL(1) FgL(1)],[cmL(2) FgL(2)],[cmL(3) FgL(3)],'d-.r',...
-        'MarkerIndices',2,'LineWidth',.75,'MarkerSize',9)
+    plot3([cmL(1) FgL(1)],[cmL(2) FgL(2)],[cmL(3) FgL(3)],...
+            'd-.r','MarkerIndices',2,'LineWidth',.75,'MarkerSize',9)
+    end
 end
 axis equal
 xlabel('x [in]'); ylabel('y [in]'); zlabel('z [in]');
