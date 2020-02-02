74e9ddb43bcfc2887e3060f48c98381442671eae
Graham Wilcox <53061984+grwilcox@users.noreply.github.com>
2020-02-01 22:22:59 -0500

    Update plotMassLoads.m
    -added documentation
    -switched to already calculated global coordinates

--- plotMassLoads.m
+++ plotMassLoads.m
@@ -1,12 +1,21 @@
-function plotMassLoads(linkArray,jointArray,T,scale)
+% Plots vectors representing weight of components over existing arm plot. 
+% >>> Must be called AFTER plotArm.
+% 
+% linkArray - structure array of link parameters
+% jointArray - structure array of joint parameters
+% scale - factor by which to scale force vector [in/N] 
+
+function plotMassLoads(linkArray,jointArray,scale)
 hold on
 for n = 1:numel(jointArray)
-    cmJ = T(n).Fi*[-jointArray(n).cmVec;1]; cmJ = cmJ(1:3);
-    cmL = T(n).Fi*[linkArray(n).cmVec;1];  cmL = cmL(1:3);
-    FgJ = cmJ + jointArray(n).gravForce/scale;
-    FgL = cmL + linkArray(n).gravForce/scale;
-    plot3([cmJ(1) FgJ(1)],[cmJ(2) FgJ(2)],[cmJ(3) FgJ(3)],'d-.r','MarkerIndices',2)
-    plot3([cmL(1) FgL(1)],[cmL(2) FgL(2)],[cmL(3) FgL(3)],'d-.r','MarkerIndices',2)
+    cmJ = jointArray(n).cmGlobal;   % coordinates of joint CoM
+    cmL = linkArray(n).cmGlobal;    % coordinates of link CoM
+    FgJ = cmJ + jointArray(n).gravForce*scale; % translate force vectors
+    FgL = cmL + linkArray(n).gravForce*scale;
+    plot3([cmJ(1) FgJ(1)],[cmJ(2) FgJ(2)],[cmJ(3) FgJ(3)],'d-.r',...
+        'MarkerIndices',2,'LineWidth',.75,'MarkerSize',9)
+    plot3([cmL(1) FgL(1)],[cmL(2) FgL(2)],[cmL(3) FgL(3)],'d-.r',...
+        'MarkerIndices',2,'LineWidth',.75,'MarkerSize',9)
 end
 axis equal
 xlabel('x [in]'); ylabel('y [in]'); zlabel('z [in]');
