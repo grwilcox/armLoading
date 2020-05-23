50789c6eb7a452c94a37eff886a3573fb27f558a
Graham Wilcox <53061984+grwilcox@users.noreply.github.com>
2020-05-23 18:43:10 -0400

    account for splitting of torque functions

--- getJointLoads.m
+++ getJointLoads.m
@@ -12,9 +12,14 @@ function torques = getJointLoads(jointArray,q0,q1,q2)
 [num,tt] = size(q0);
 torques = zeros(num,tt,3);
 for n = 1:num
-    f = jointArray(n).torques;
+%     if n ~= 3 % TODO: yeah why is the elbow torque running out of memory?
+%     f = jointArray(n).torques;
+    g = jointArray(n).gravT;
+    v = jointArray(n).veloT;
+    a = jointArray(n).accelT;
     for t = 1:tt
-        torques(n,t,:) = f(q0(:,t),q1(:,t),q2(:,t));
+        torques(n,t,1) = g(q0(:,t),q1(:,t),q2(:,t));
+        torques(n,t,2) = v(q0(:,t),q1(:,t),q2(:,t));
+        torques(n,t,3) = a(q0(:,t),q1(:,t),q2(:,t));
     end
-end
 end
