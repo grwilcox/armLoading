d13121c7523066d3dd7114d24d8cb1f3a315da38
Graham Wilcox <53061984+grwilcox@users.noreply.github.com>
2020-05-23 18:51:45 -0400

    add CoM and Inertia matrix inputs

--- jointBuilder.m
+++ jointBuilder.m
@@ -3,20 +3,30 @@
 % 
 % name - string, plain identifier for joint
 % id - int, unique numerical identifier for joint, the i of S_i
-% mass - mass of component in kg
 % length - distance S_j between link vectors a_ij and a_jk
+% mass - mass of all parts comoving with reference frame [kg]
+% CoM - center of mass displacement from origin of reference frame
+% Inertia - (CoM) moment of inertia matrix of all elements in frame
+% axes - mapping from [x,y,z] axes of CoM and inertia (from CAD) to axes of
+% joint reference frame. link vector a is x', joint vector S is z'
 % angle - skew angle theta_j between link vectors a_ij and a_jk
+% velocity - angular velocity of joint
+% acceleration - angular acceleration of joint
 % k - struct of gravity parameters
 %
 % joint - struct of characteristics of the single joint at hand
 
-function joint = jointBuilder(name, id, mass, length, angle, k)
+function joint = jointBuilder(name, id, length, mass, CoM,...
+    Inertia, axes, angle, velocity, acceleration, k)
 joint.name = name;
 joint.id = id;
-joint.mass = mass;
 joint.S = length*k.m_in;
-joint.localVec = [0; 0; joint.S];
+joint.mass = mass;
+joint.cmLocal = [CoM(axes(1)); CoM(axes(2)); CoM(axes(3))];
+joint.Inertia = parallelAxis(Inertia,CoM,mass,axes);
 joint.theta = angle;
-joint.cmLocal = joint.localVec/2;   % TODO: add cmLocal input argument
+joint.velocity = velocity;
+joint.acceleration = acceleration;
+joint.localVec = [0; 0; joint.S];
 joint.gravForce = joint.mass * k.g * -k.kHat;
 end
