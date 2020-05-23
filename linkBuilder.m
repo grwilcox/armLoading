a926bc048e9690316494a0064d8e09b185de0056
Graham Wilcox <53061984+grwilcox@users.noreply.github.com>
2020-05-23 18:56:39 -0400

    removed mass parameter

--- linkBuilder.m
+++ linkBuilder.m
@@ -3,20 +3,16 @@
 % 
 % name - string, plain identifier for link
 % id - int, unique numerical identifier for link, the i of a_ij
-% mass - mass of component in kg
 % length - distance a_ij between joint vectors S_i and S_j
 % angle - skew angle alpha_ij between joint vectors S_i and S_j
 % k - struct of gravity parameters
 %
 % link - struct of characteristics of the single link at hand
 
-function link = linkBuilder(name, id, mass, length, angle, k)
+function link = linkBuilder(name, id, length, angle, k)
 link.name = name;
 link.id = id;
-link.mass = mass;
 link.a = length*k.m_in;
 link.localVec = [link.a; 0; 0];
 link.alpha = angle;
-link.cmLocal = link.localVec/2; % TODO: add cmLocal input argument
-link.gravForce = link.mass * k.g * -k.kHat;
 end
