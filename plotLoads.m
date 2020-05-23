4d86da96ad439e259c73ee61b5a342d72faff0b3
Graham Wilcox <53061984+grwilcox@users.noreply.github.com>
2020-05-23 19:01:34 -0400

    add 'total' option to plot all joints

--- plotLoads.m
+++ plotLoads.m
@@ -5,16 +5,29 @@
 %           [jointID, timestep, load component]
 % tt - array of timesteps
 % jointArray - structure array of joint parameters
-% n - ID specify which joint to plot loads of
+% n - ID specify which joint to plot loads of 
+%       OR 'total' for plot of total torques for all joints
 
 function plotLoads(loads,tt,jointArray,n)
 figure(nextFig)
 hold on
 xlabel('time [s]')
 ylabel('Torque [N*m]')
-plot(tt,loads(n,:,1))
-plot(tt,loads(n,:,2))
-plot(tt,loads(n,:,3))
-plot(tt,sum(loads(n,:,:),3))
-legend('gravity','velocity','acceleration','total')
-title(['Loads on ' jointArray(n).name])
+if isnumeric(n)
+    plot(tt,loads(n,:,1))
+    plot(tt,loads(n,:,2))
+    plot(tt,loads(n,:,3))
+    plot(tt,sum(loads(n,:,:),3))
+    legend('gravity','velocity','acceleration','total')
+    title(['Loads on ' jointArray(n).name])
+elseif n == 'total'
+    for i = 1:size(loads,1)
+        plot(tt,sum(loads(i,:,:),3))
+    end
+    legend(jointArray(1).name,jointArray(2).name,jointArray(3).name,...
+        jointArray(4).name,jointArray(5).name,jointArray(6).name)
+    title('Total loads on each joint')
+else
+    error('no');
+end
+end
